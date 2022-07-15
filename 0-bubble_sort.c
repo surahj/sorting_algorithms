b976dd6739569b3bed1d4d6d5609549c20324549
surahj <odebodeopeyemi@gmail.com>
2022-07-15 18:43:49 +0100

    Update

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array in ascending order
+ * @array: array of element to sort
+ * @size: size of the elemet to sort
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorth the element of a linked list
+ * @list: pointer to first node of the list
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = (*list)->next, *current_node, *previous_node;
+
+	if (!list || !*list || !((*list)->next))
+		return;
+
+	while (temp != NULL)
+	{
+		previous_node = temp->prev;
+		current_node = temp;
+		temp = temp->next;
+
+		while ((previous_node != NULL) && previous_node->n > current_node->n)
+		{
+			swap_node(list, &previous_node, current_node);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
+
+/**
+ * swap_node - swap two another
+ * @h: pointer to first node of double linked list
+ * @first_node: first node to swap
+ * @second_node: second node to swap
+ * Return: Nothing
+ */
+
+void swap_node(listint_t **h, listint_t **first_node, listint_t *second_node)
+{
+	(*first_node)->next = second_node->next;
+	if (second_node->next != NULL)
+		second_node->next->prev = *first_node;
+
+	second_node->prev = (*first_node)->prev;
+	if ((*first_node)->prev != NULL)
+		(*first_node)->prev->next = second_node;
+
+	else
+		*h = second_node;
+
+	(*first_node)->prev = second_node;
+	second_node->next = *first_node;
+	*first_node = second_node->prev;
+}
