3dccf953bf63547c013f17413e237df004a76f54
surahj <odebodeopeyemi@gmail.com>
2022-07-19 21:49:42 +0100

    Update

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void insertion_sort_list(listint_t **list);
 void swap_node(listint_t **h, listint_t **first_node, listint_t *second_node);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
