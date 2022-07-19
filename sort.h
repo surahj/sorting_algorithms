44b7ae8bace174e57477de96c5711eed724bd3cd
surahj <odebodeopeyemi@gmail.com>
2022-07-19 22:05:19 +0100

    Update

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,5 +26,6 @@ void swap_node(listint_t **h, listint_t **first_node, listint_t *second_node);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
