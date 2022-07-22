823237378fe8594ac0eb50aa03d64eb550c12dca
surahj <odebodeopeyemi@gmail.com>
2022-07-22 15:09:06 +0100

    Update

--- sort.h
+++ sort.h
@@ -49,5 +49,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-
+void quick_sort_hoare(int *array, size_t size);
 #endif
