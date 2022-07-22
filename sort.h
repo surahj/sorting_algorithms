295964efc11f75679f5a513b37cfb6c30635db23
surahj <odebodeopeyemi@gmail.com>
2022-07-22 15:04:50 +0100

    Update

--- sort.h
+++ sort.h
@@ -24,6 +24,17 @@ typedef struct listint_s
 #define UP 0
 #define DOWN 1
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
