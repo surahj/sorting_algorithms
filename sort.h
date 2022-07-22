409670b70717c20f8b54f7766754373633745c54
surahj <odebodeopeyemi@gmail.com>
2022-07-22 14:59:25 +0100

    Update

--- sort.h
+++ sort.h
@@ -37,5 +37,6 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
